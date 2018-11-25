2ea4d575781fc3af85eb42d5cd727a520bdc430e
Vadim <vadimgromovoy@gmail.com>
2018-11-25 17:34:19 +0300

    Table data upload

--- database/migrations/2018_11_25_143010_basket.php
+++ database/migrations/2018_11_25_143010_basket.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Basket extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('order', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->decimal('bonus');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->integer('id_plate');
+            $table->integer('id_user');
         });
     }
 
@@ -30,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('basket');
     }
 }
