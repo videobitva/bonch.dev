bb6ce340dc7b8d0b72de4f2e05653b855808e598
videobitva <vadimgromovoy@gmail.com>
2018-12-16 20:58:05 +0300

    Update Plate.php

--- app/Models/Plate.php
+++ app/Models/Plate.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace App;
 use Illuminate\Database\Eloquent\Model;
+
 /**
  * Class Plate
  *
@@ -37,28 +39,37 @@ class Plate extends Model
      * Plate constructor.
      * @param array $attributes
      */
+
     public function cart(){
         return $this->belongsTo('Cart::class');
     }
+
     public function genre(){
         return $this->belongsTo('App\Models\Genre');
     }
+
     public function label(){
         return $this->belongsTo('App\Models\Label');
     }
+
     public function order(){
         return $this->belongsTo('Order::class');
     }
+
     public function singer(){
         return $this->belongsTo('App\Models\Singer');
     }
+
     public function state()
     {
         return $this->belongsTo('App\Models\State');
+
     }
+
         public function country(){
             return $this->belongsTo('App\Models\Country');
         }
+
     protected $table = 'plates';
     public $timestamps = false;
 }
