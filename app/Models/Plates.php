d87e2924839320ac61b836f2795227a16b42a54c
videobitva <vadimgromovoy@gmail.com>
2018-12-16 20:55:26 +0300

    Create Plates.php

---
+++ app/Models/Plates.php
@@ -0,0 +1,56 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+/**
+ * App\Plate
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate query()
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $name
+ * @property int $id_singer
+ * @property int $id_genre
+ * @property int $year
+ * @property int $id_country
+ * @property int $id_state
+ * @property float $price
+ * @property int $count
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate whereCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate whereIdCountry($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate whereIdGenre($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate whereIdSinger($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate whereIdState($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Plate whereYear($value)
+ */
+class Plate extends Model
+{
+    public function genre(){
+        return $this->belongsTo('App\Genre');
+    }
+    public function label(){
+        return $this->belongsTo('App\Label');
+    }
+    public function order(){
+        return $this->belongsTo('App\Order');
+    }
+    public function singer(){
+        return $this->belongsTo('App\Singer');
+    }
+    public function state(){
+        return $this->belongsTo('App\State');
+    }
+    public function country(){
+        return $this->belongsTo('App\Country');
+    }
+    protected $table = 'plates';
+    public $timestamps = false;
+}
