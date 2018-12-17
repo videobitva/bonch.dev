06a1aeb65cfb6e9f0980d93573b87233e68dc42f
Vadim <vadimgromovoy@gmail.com>
2018-12-17 17:40:27 +0300

    Route fix

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,14 @@ Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
 
-Route::group(['middleware' => ['web']], function() {
+Route::get('/cart/add', 'CartController@actionAdd');
+Route::get('/cart/delete', 'CartController@actionDelete');
+Route::get('/cart/count', 'CartController@actionCount');
+Route::get('/cart/show', 'CartController@actionShow');
 
-    Route::get('/cart/add', 'CartController@actionAdd');
-    Route::get('/cart/delete', 'CartController@actionDelete');
-    Route::get('/cart/count', 'CartController@actionCount');
-    Route::get('/cart/show', 'CartController@actionShow');
-
-});
 
 Route::group(['middleware' => ['web', 'auth', 'verified']], function (){
+
     Route::get('/user/get_favourite', 'UserController@getFavourite');
     Route::get('/user/add_favourite', 'UserController@addFavourite');
     Route::get('/user/remove_favourite', 'UserController@removeFavourite');
@@ -35,6 +33,7 @@ Route::group(['middleware' => ['web', 'auth', 'verified']], function (){
     Route::get('/order/make', 'OrderController@order');
     Route::get('/order/index', 'OrderController@index');
     Route::get('/order/total', 'OrderController@total');
+
 });
 
 Route::get('/plate/genre/','PlateController@sortGenre');
